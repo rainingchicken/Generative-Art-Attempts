d734b02bc7b23777a93dfd982dfd1cd308ad0bae
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2023-12-27 06:23:08 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+  var circleSize; //controls size of circle
+  var circleColor; //controls filled color of the cicle created
+}
+
+function draw() {
+  circleColor = random(random(0, 255), random(0, 255), random(0, 255)); //colors set to random for visual simulation
+  circleSize = random(10, 200); //size is also random to make it interesting
+
+  if (mouseIsPressed == true) {
+    //creates circles when  mouse is clicked
+    fill(circleColor);
+    circle(mouseX, mouseY, circleSize); //creates circle shape
+  }
+}
