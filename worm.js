8443be72c224051f6eed4425fffd4038b429a32e
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2023-12-27 22:26:55 -0800

    Update and rename sketch2.js to worm.js

--- worm.js
+++ worm.js
@@ -1,13 +1,12 @@
 function setup() {
-  let cnv2 = createCanvas(400, 400);
-  cnv2.position(0, 500, 'absolute');
+  createCanvas(400, 400);
   var circleSize; //controls size of circle
   var circleColor; //controls filled color of the cicle created
 }
 
 function draw() {
-  circleColor = random(random(0, 255), random(0, 255), random(0, 255)); //colors set to random for visual simulation
-  circleSize = random(10, 200); //size is also random to make it interesting
+  circleColor = random(random(0, 255), random(0, 255)); //colors set to random for visual simulation
+  circleSize = random(1, 150); //size is also random to make it interesting
 
   if (mouseIsPressed == true) {
     //creates circles when  mouse is clicked
